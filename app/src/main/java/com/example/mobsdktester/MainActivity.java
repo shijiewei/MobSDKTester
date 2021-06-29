c998060f8a3abe11b143e3e1eff49b424cb84198
shijiewei <jackiewayne2046@163.com>
2021-06-29 10:52:32 +0800

    添加隐私接口调用

--- app/src/main/java/com/example/mobsdktester/MainActivity.java
+++ app/src/main/java/com/example/mobsdktester/MainActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.mylib.MyLib;
+import com.mob.MobSDK;
 
-//import cn.smssdk.gui.RegisterPage;
+import cn.smssdk.gui.RegisterPage;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 	private Button goToSecBtn;
 	private Button goToSmsBtn;
+	private Button btnAgreePolicy;
+	private Button btnInvokeIsForb;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		goToSecBtn.setOnClickListener(this);
 		goToSmsBtn = findViewById(R.id.main_add_go_to_sms_btn);
 		goToSmsBtn.setOnClickListener(this);
+		btnAgreePolicy = findViewById(R.id.main_btn_agree_policy);
+		btnAgreePolicy.setOnClickListener(this);
+		btnInvokeIsForb = findViewById(R.id.main_btn_invoke_isforb);
+		btnInvokeIsForb.setOnClickListener(this);
 	}
 
 	@Override
@@ -33,8 +41,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		if (id == R.id.main_go_to_my_lib) {
 			MyLib.goToMyLibActivity(this);
 		} else if (id == R.id.main_add_go_to_sms_btn) {
-//			RegisterPage page = new RegisterPage();
-//			page.show(this);
+			RegisterPage page = new RegisterPage();
+			page.show(this);
+		} else if (id == R.id.main_btn_agree_policy) {
+			MobSDK.submitPolicyGrantResult(true, null);
+		} else if (id == R.id.main_btn_invoke_isforb) {
+			boolean isForb = MobSDK.isForb();
+			Toast.makeText(this, "isForb: " + isForb, Toast.LENGTH_SHORT).show();
 		}
 	}
 }
